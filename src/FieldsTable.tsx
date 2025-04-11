2b0c3568b8852494de04eb949b380b43281894e5
j- <j@skeoh.com>
2025-04-11 10:28:08 +1000

    Add copy button for values

--- src/FieldsTable.tsx
+++ src/FieldsTable.tsx
@@ -19,18 +19,37 @@ export const FieldsTable: FC<FieldsTableProps> = ({ fields }) => {
         {fields.map(([name, value], i) => (
           <tr key={`${i}-${name}`}>
             <th scope="row">
-              <button
-                type="button"
-                className="btn btn-link p-0 font-monospace text-black fw-bold"
-                onClick={() => navigator.clipboard.writeText(value)}
-                title={`Click to copy the value of "${name}" to clipboard`}
-              >
-                {name}
-              </button>
+              <div className="d-flex flex-column h-100 mh-100 justify-space-between align-items-start gap-2 lh-base">
+                <pre className="p-0 m-0 font-monospace text-black fw-bold text-start lh-base">
+                  {name}
+                </pre>
+
+                <button
+                  type="button"
+                  className="btn btn-link btn-sm p-0 m-0 text-body-tertiary fw-bold text-start lh-base"
+                  onClick={() => navigator.clipboard.writeText(name)}
+                  title={`Click to copy "${name}" to clipboard`}
+                >
+                  Copy
+                </button>
+              </div>
             </th>
             <td>
-              <span className="FieldsTable-value">{value}</span>
-              <span className="FieldsTable-length">{` (${value.length})`}</span>
+              <div className="d-flex flex-column h-100 mh-100 justify-space-between align-items-start gap-2 lh-base">
+                <div className="lh-base">
+                  <span className="FieldsTable-value font-monospace">{value}</span>
+                  <span className="FieldsTable-length">{` (${value.length})`}</span>
+                </div>
+
+                <button
+                  type="button"
+                  className="btn btn-link btn-sm p-0 m-0 text-body-tertiary fw-bold text-start lh-base"
+                  onClick={() => navigator.clipboard.writeText(value)}
+                  title={`Click to copy the value of "${name}" to clipboard`}
+                >
+                  Copy
+                </button>
+              </div>
             </td>
           </tr>
         ))}
